38082b5725e7ef01312fb88053fd743530f0f7da
Nicco <nicconike6@gmail.com>
2024-04-27 02:51:19 +0530

    Add app.py

---
+++ api/app.py
@@ -0,0 +1,32 @@
+"""Flask Application"""
+from flask import Flask, jsonify
+from steam_stats import get_player_summaries, get_recently_played_games
+from steam_workshop import fetch_workshop_item_links, fetch_all_workshop_stats
+
+app = Flask(__name__)
+
+
+@app.route("/player-summaries")
+def player_summaries():
+    """Endpoint to fetch player summaries"""
+    data = get_player_summaries()
+    return jsonify(data)
+
+
+@app.route("/recently-played-games")
+def recently_played_games():
+    """Endpoint to fetch recently played games"""
+    data = get_recently_played_games()
+    return jsonify(data)
+
+
+@app.route("/workshop-stats/<steam_id>")
+def workshop_stats(steam_id):
+    """Endpoint to fetch workshop stats for a given Steam ID"""
+    links = fetch_workshop_item_links(steam_id)
+    data = fetch_all_workshop_stats(links)
+    return jsonify(data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- api/steam_stats.py
+++ api/steam_stats.py
@@ -84,32 +84,3 @@ def process_player_summary_data(player_data):
         "gameid": player_data.get("gameid")
     }
     return process_data
-
-# def generate_svg_for_recently_played_games(player_data):
-#     """Generate SVG for Recently Played Games in Steam in the last 2 weeks"""
-#     bar_chart = pygal.HorizontalBar(
-#         legend_at_bottom=True, rounded_bars=15, show_legend=True)
-#     bar_chart.title = "Playtime in the Last Two Weeks (hours)"
-
-#     # Add data to the chart
-#     if player_data and "response" in player_data and "games" in player_data["response"]:
-#         for game in player_data["response"]["games"]:
-#             if "name" in game and "playtime_2weeks" in game:
-#                 playtime_minutes = game["playtime_2weeks"]
-#                 playtime_hours = playtime_minutes / 60  # Convert minutes to hours for plotting
-
-#                 # Determine the label based on the original playtime in minutes
-#                 if playtime_minutes >= 60:
-#                     # Display in hours if 60 mins or more
-#                     label = f"{game["name"]} ({playtime_hours:.2f} hrs)"
-#                 else:
-#                     # Display in minutes if less than 60
-#                     label = f"{game["name"]} ({playtime_minutes} mins)"
-
-#                 # Add to chart using the hours value for consistency in scaling
-#                 bar_chart.add(label, playtime_hours)
-#     else:
-#         print("No game data available to display")
-
-#     # Return the SVG data as a string to embed directly in Markdown
-#     return bar_chart.render(is_unicode=True)
