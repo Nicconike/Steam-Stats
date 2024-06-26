7e63045fdead263d24c0ef527821794c34393db2
Nicconike <38905025+Nicconike@users.noreply.github.com>
2024-06-26 21:26:48 +0530

    fix(card): This commit fixes the bug to handle the scenario where the player has not played any games in last 2 weeks

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,4 @@
+"""Declares this module as a package"""
 # tests/__init__.py
 
 # This file can be empty or contain initialization code for the package
-import logging
-
-# comment to enable logging w/ tests
-logging.disable(logging.CRITICAL)
