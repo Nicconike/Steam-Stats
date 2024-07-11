f7002c87b12fa9a9473aebb7e1d6386de29ddd27
semantic-release <semantic-release>
2024-07-11 23:44:50 +0000

    chore: Version Release v1.0.2

--- api/main.py
+++ api/main.py
@@ -29,7 +29,7 @@ WORKSHOP_STATS = os.getenv("INPUT_WORKSHOP_STATS",
                            "false").lower() in ("true", "1", "t")
 
 # Version Identifier for Changelog
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 
 
 def update_readme(repo, markdown_data, start_marker, end_marker):
